b6085308909367763052b4858dc3c22e8229db8e
anhdat96 <49085300+anhdat96@users.noreply.github.com>
2021-07-04 18:45:08 +0700

    add file

--- currency-conversion-service/src/main/java/microservice/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/microservice/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,13 @@ package microservice.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("microservice.currencyconversionservice")
+@EnableDiscoveryClient
+// register with eureka
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
--- netflix-zulu-gateway-server/src/main/java/microservice/netflixzulugatewayserver/fillter/ZuulLoggingFilter.java
+++ netflix-zulu-gateway-server/src/main/java/microservice/netflixzulugatewayserver/fillter/ZuulLoggingFilter.java
@@ -1,7 +1,41 @@
 package microservice.netflixzulugatewayserver.fillter;
 
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.http.HttpRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Component
-public class ZuulLogginFilter {
+public class ZuulLoggingFilter extends ZuulFilter {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public String filterType() {
+        // filter all request before execute
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        // logic come here
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+
+        logger.info("request --> {} , request uri --> {}",request,request.getRequestURI());
+        return null;
+    }
 }
