6d73f384dd16c1db17f7e06ad3e9f6ab1b0d4759
anhdat96 <49085300+anhdat96@users.noreply.github.com>
2021-07-04 18:44:24 +0700

    test

--- currency-exchange-service/src/main/java/microservice/currencyexchangeservice/entity/ExchangeValue.java
+++ currency-exchange-service/src/main/java/microservice/currencyexchangeservice/entity/ExchangeValue.java
@@ -1,4 +1,32 @@
 package microservice.currencyexchangeservice.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "exchange_value")
 public class ExchangeValue {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "exchange_from")
+    private String exchangeFrom;
+
+    @Column(name = "exchange_to")
+    private String exchangeTo;
+
+    @Column(name = "conversion_multiple")
+    private BigDecimal conversionMultiple;
+
+    @Column(name = "port")
+    private int port;
 }
--- currency-exchange-service/src/main/java/microservice/currencyexchangeservice/repository/ExchangeServiceRepository.java
+++ currency-exchange-service/src/main/java/microservice/currencyexchangeservice/repository/ExchangeServiceRepository.java
@@ -1,4 +1,11 @@
 package microservice.currencyexchangeservice.repository;
 
-public interface ExchangeServiceRepository {
+import microservice.currencyexchangeservice.entity.ExchangeValue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExchangeServiceRepository extends JpaRepository<ExchangeValue,Long> {
+
+    ExchangeValue findByExchangeFromAndExchangeTo(String from,String to);
 }
